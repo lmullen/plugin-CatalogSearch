e7b8e51696fcd34c2bf2d01659476889d3f2a95b
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-05-03 19:35:10 -0400

    Add title to edit page

---
+++ views/admin/index/edit.php
@@ -0,0 +1,8 @@
+<?php 
+
+$head = array('bodyclass' => 'simple-pages primary',
+              'title' => __('Simple Pages | Edit "%s"', metadata('catalog_search_search', 'catalog_name' )));
+
+echo head($head);
+
+?>
